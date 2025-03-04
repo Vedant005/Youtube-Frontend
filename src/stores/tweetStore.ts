5b33173581635b06a22406386196a825c41bd3c8
Vedant005 <vedantkanekar909@gmail.com>
2025-03-04 22:46:41 +0530

    feat: add create, update, and delete functionality to tweet store

--- src/stores/tweetStore.ts
+++ src/stores/tweetStore.ts
@@ -14,11 +14,19 @@ interface Tweet {
   isLiked: boolean;
 }
 
+interface Update {
+  content: string;
+  tweetId: string;
+}
+
 interface TweetStore {
   tweets: Tweet[];
   loading: boolean;
   error: string | null;
   getUserTweets: () => Promise<void>;
+  createTweet: (content: string) => Promise<void>;
+  updateTweet: (credentials: Update) => Promise<void>;
+  deleteTweet: (tweetId: string) => Promise<void>;
 }
 
 const useTweetStore = create<TweetStore>()(
@@ -34,6 +42,47 @@ const useTweetStore = create<TweetStore>()(
         try {
           const response = await apiClient.get("/tweets/user");
           set({ tweets: response.data.data, loading: false });
+        } catch (error: any) {
+          set({
+            loading: false,
+            error: error.message || "Failed to fetch tweets",
+          });
+        }
+      },
+      createTweet: async (content) => {
+        set({ loading: true, error: null });
+
+        try {
+          const response = await apiClient.post("/tweets/", { content });
+          set({ loading: false });
+        } catch (error: any) {
+          set({
+            loading: false,
+            error: error.message || "Failed to fetch tweets",
+          });
+        }
+      },
+
+      updateTweet: async (credentials) => {
+        set({ loading: true, error: null });
+
+        try {
+          const response = await apiClient.patch("/tweets/", { credentials });
+          set({ loading: false, error: null });
+        } catch (error: any) {
+          set({
+            loading: false,
+            error: error.message || "Failed to fetch tweets",
+          });
+        }
+      },
+
+      deleteTweet: async (tweetId) => {
+        set({ loading: true, error: null });
+
+        try {
+          await apiClient.delete(`/tweets/${tweetId}`);
+          set({ loading: false, error: null });
         } catch (error: any) {
           set({
             loading: false,
